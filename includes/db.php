0a401748638397027140b58ac974e69dc1dd621f
tamonwan1222 <574407076@parichat.skru.ac.th>
2017-07-26 14:59:29 +0700

    Add test file"

--- includes/db.php
+++ includes/db.php
@@ -2,8 +2,8 @@
 
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
-$db['db_pass'] = "root";
-$db['db_name'] = "cms";
+$db['db_pass'] = "123456";
+$db['db_name'] = "git";
 
 foreach($db as $key => $value){
 define(strtoupper($key), $value);
